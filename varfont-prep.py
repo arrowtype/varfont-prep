86fd3ee80a5cae1acdeb1017856516a9af953b3e
Stephen Nixon <swr410@gmail.com>
2018-04-28 18:17:13 +0200

    make lists of glyphs in each copied font

--- varfont-prep.py
+++ varfont-prep.py
@@ -5,11 +5,15 @@ import os
 import shutil
 import datetime
 
-
-
 # take in multiple fonts
 inputFonts = getFile("select masters for var font", allowsMultipleSelection=True, fileTypes=["ufo"])
 
+# def generateReport(inputFonts, errors):
+#     newFolderPath = makeVarFontPrepFolder(inputFonts)
+    
+#     report = open(newFolderPath + "/" + 'varfontprep-report.txt','w')
+#     report.write(str(errors))
+#     report.close()
 
 def checkIfSameFamilyName(inputFonts):
     fontFamilyNames = []
@@ -25,72 +29,96 @@ def checkIfSameFamilyName(inputFonts):
         return sameName, fontFamilyNames[0]
     else:
         errorMsg = "The input UFOs have different font family names: " + str(set(fontFamilyNames))
-        return errorMsg
+        generateReport(inputFonts, errorMsg)
+        # return errorMsg
+        
 
-def copyFonts(newFolderPath, inputFonts):
+def copyFonts(inputFonts, newFolderPath):
     for fontPath in inputFonts:
         head, tail = os.path.split(fontPath)
         
         # copy UFO into newFolderPath
         shutil.copytree(fontPath, newFolderPath + "/varprep-" + tail)
 
-def duplicateFontsToFolder(inputFonts):
+def makeVarFontPrepFolder(inputFonts):
     
-    # check that selected fonts have same family name
-    if checkIfSameFamilyName(inputFonts)[0] == True:
+    # # check that selected fonts have same family name
+    # if checkIfSameFamilyName(inputFonts)[0] == True:
         
-        # get family name
-        familyName = checkIfSameFamilyName(inputFonts)[1]
-        
-        # make new folder name with font family name and "varfontprep" label
-        newFolderName = familyName.replace(" ","_").lower() + "-varfontprep"
-        
-        # get path of first input font
-        path = inputFonts[0]
-        
-        # get head of font path
-        head, tail = os.path.split(path)
+    # get family name
+    familyName = checkIfSameFamilyName(inputFonts)[1]
+    
+    # make new folder name with font family name and "varfontprep" label
+    newFolderName = familyName.replace(" ","_").lower() + "-varfontprep"
+    
+    # get path of first input font
+    path = inputFonts[0]
+    
+    # get head of font path
+    head, tail = os.path.split(path)
 
-        # make new folder path
-        newFolderPath = head + "/" + newFolderName
+    # make new folder path
+    newFolderPath = head + "/" + newFolderName
 
-        if not os.path.exists(newFolderPath):
-            os.mkdir(newFolderPath)
-            
-            copyFonts(newFolderPath, inputFonts)
-        else:
-            # add current date & time to folder path if the base name is already taken
-            now = datetime.datetime.now()
-            newFolderPath += "-" + now.strftime("%Y_%m_%d-%H_%M_%S")
-            os.mkdir(newFolderPath)
-            
-            copyFonts(newFolderPath, inputFonts)
-            
-
-    # if font family names are different, print the returned error
+    if not os.path.exists(newFolderPath):
+        os.mkdir(newFolderPath)
+        print(newFolderPath)
+        return str(newFolderPath)
     else:
-        print(checkIfSameFamilyName(inputFonts))
+        # add current date & time to folder path if the base name is already taken
+        now = datetime.datetime.now()
+        newFolderPath += "-" + now.strftime("%Y_%m_%d-%H_%M_%S")
+        os.mkdir(newFolderPath)
+        print(newFolderPath)
+        return str(newFolderPath)
 
-duplicateFontsToFolder(inputFonts)
+    # # if font family names are different, print the returned error
+    # else:
+    #     print(checkIfSameFamilyName(inputFonts))
+    #     print("sorry, fonts have different family names")
 
-# duplicate these fonts, deleting all glyphs which can not be interpolated
 
-    # check that font info matches
-        # if not, 
-            # send report 
-            # stop process 
+def duplicateFontsToFontPrepFolder(inputFonts, newFolderPath):
+    # # check that selected fonts have same family name
+    if checkIfSameFamilyName(inputFonts)[0] == True:
+        # newFolderPath = makeVarFontPrepFolder(inputFonts)
+    
+        print(newFolderPath)
+    
+        copyFonts(inputFonts, newFolderPath)
+    else:
+        print("sorry, fonts have different family names")
 
-    # duplicate UFOs into new folder 
-        # folderName = familyName + varfontprep
+newFolderPath = makeVarFontPrepFolder(inputFonts)
+duplicateFontsToFontPrepFolder(inputFonts, newFolderPath)
 
-    # make a list of drawn glyphs in each
-    # commonGlyphs = compare lists and come up with new list containing common items
+######################################### 
+######### make fonts compatible #########
+#########################################
 
-    # for each item in commonGlyphs
-    # count contours and points 
-    # if there is a mismatch, delete glyph froom each input file and 
-    # send "point mismatch" notice to report generator
+glyphLists = []
 
-# also generate a report of what glyphs can not be interpolated, and specify why not
-    # different number of points
-    # points/ contours out of order
+for fontFile in os.listdir(newFolderPath):
+    print(fontFile)
+    fullFontPath = newFolderPath + "/" + fontFile 
+    print(fullFontPath)
+    f = OpenFont(fullFontPath, showUI=False)
+    fontName = f.info.familyName + " " + f.info.styleName
+    
+    print(fontName)
+    
+    glyphs = []
+    
+    for g in f:
+        glyphs.append(g.name)
+    
+    glyphLists.append(glyphs)
+
+print(glyphLists)
+    
+
+
+
+
+
+
