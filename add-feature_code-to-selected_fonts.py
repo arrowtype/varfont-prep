83da35e495c2f052c6223d95d9e13983d8a77485
Stephen Nixon <stephen@thundernixon.com>
2019-06-04 19:51:46 -0400

    add feature-code-adding script

---
+++ add-feature_code-to-selected_fonts.py
@@ -0,0 +1,24 @@
+from vanilla.dialogs import *
+
+
+inputFonts = getFile("select masters to add feature code to",
+                     allowsMultipleSelection=True, fileTypes=["ufo"])
+
+
+feaText = """\
+languagesystem DFLT dflt;
+languagesystem latn dflt;
+"""
+
+
+def addFeatureCode(f):
+    f.features.text = feaText
+
+
+for fontPath in inputFonts:
+    f = OpenFont(fontPath, showInterface=False)
+    addFeatureCode(f)
+    fontName = f.info.familyName + " " + f.info.styleName
+    print("feature code added to " + fontName)
+    f.save()
+    f.close()
