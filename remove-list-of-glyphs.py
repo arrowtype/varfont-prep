52563f3204fbefbf97711df5bbfd0af57ca36cae
Stephen Nixon <stephen@thundernixon.com>
2019-06-04 18:45:56 -0400

    allow user to remove copied list from fontmake error output

--- remove-list-of-glyphs.py
+++ remove-list-of-glyphs.py
@@ -5,13 +5,15 @@ from vanilla.dialogs import *
 # copy-paste to fill this list with whatever glyphs fontMake flags as not being interpolatable
 # glyphsToDelete = ['LISTOFGLYPHSHERE']
 
-glyphsToDelete = AskString('Space-separated set of glyphnames to delete').split(" ")
+glyphsToDelete = AskString(
+    'Space-separated set of glyphnames to delete').replace("'", "").replace(",", "").split(" ")
 
 # help(CurrentFont().removeGlyph())
 
 instruction = f"select masters to remove {glyphsToDelete} from"
 
-inputFonts = getFile(instruction, allowsMultipleSelection=True, fileTypes=["ufo"])
+inputFonts = getFile(
+    instruction, allowsMultipleSelection=True, fileTypes=["ufo"])
 
 for fontPath in inputFonts:
     f = OpenFont(fontPath, showInterface=False)
@@ -23,4 +25,3 @@ for fontPath in inputFonts:
     print("done!")
     f.save()
     f.close()
-
